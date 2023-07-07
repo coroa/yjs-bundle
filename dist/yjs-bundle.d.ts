4851bf24264d422e124e17b54c4b5700360936e2
Jonas Hoersch <jonas.hoersch@climateanalytics.org>
2023-07-07 19:21:07 +0200

    Update dist

--- dist/yjs-bundle.d.ts
+++ dist/yjs-bundle.d.ts
@@ -5,4 +5,5 @@ import { WebrtcConn, Room, SignalingConn, WebrtcProvider } from 'y-webrtc';
 import { YKeyValue } from 'y-utility/y-keyvalue';
 import { LWWMap } from 'y-lwwmap';
 import { Observable } from 'lib0/observable';
-export { PREFERRED_TRIM_SIZE, fetchUpdates, storeState, clearDocument, IndexeddbPersistence, WebsocketProvider, messageAuth, messageAwareness, messageQueryAwareness, messageSync, WebrtcConn, Room, SignalingConn, WebrtcProvider, YKeyValue, LWWMap, Observable };
+import * as syncedStore from '@syncedstore/core';
+export { PREFERRED_TRIM_SIZE, fetchUpdates, storeState, clearDocument, IndexeddbPersistence, WebsocketProvider, messageAuth, messageAwareness, messageQueryAwareness, messageSync, WebrtcConn, Room, SignalingConn, WebrtcProvider, YKeyValue, LWWMap, Observable, syncedStore };
