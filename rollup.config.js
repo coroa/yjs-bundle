feda083219cfc5fe6f58c7d45dee20daf8e014f7
Jonas Hoersch <jonas.hoersch@climateanalytics.org>
2023-07-07 18:46:40 +0200

    Update versions and include @syncedstore/core

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@
 import commonjs   from '@rollup/plugin-commonjs'
 import resolve    from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript';
-import { terser } from 'rollup-plugin-terser'
+import terser from '@rollup/plugin-terser'
 
 export default {
   input:    './src/yjs-bundle.ts',
